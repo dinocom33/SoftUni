4bd817ab79254c73c8dc946a70fdffceb9e86923
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2022-08-30 15:46:13 +0300

    Delete Programming Basics with Python/First Step in Progemming directory

--- Programming Basics with Python/First Step in Progemming/Lab/yard_greening.py
+++
@@ -1,9 +0,0 @@
-sq_meters = float(input())
-price_per_sq_meters = 7.61
-price_without_disc = sq_meters * price_per_sq_meters
-price_with_disc = price_without_disc * 0.82
-
-final_price = (sq_meters * price_per_sq_meters)
-print(f"The final price is {price_with_disc} lv.")
-discount = (price_without_disc - price_with_disc)
-print(f"The discount is {discount} lv.")
