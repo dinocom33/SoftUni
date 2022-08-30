08f5ad0a3038b4d1840cc8aa9ba3190e3f1ca746
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2022-08-30 15:53:58 +0300

    Create cinema.py

---
+++ Programming Basics with Python/3 Conditional Statements Advanced/2 Exercises/cinema.py
@@ -0,0 +1,15 @@
+screening_type = input()
+rows = int(input())
+columns = int(input())
+
+income = 0
+capacity = rows * columns
+
+if screening_type == "Premiere":
+    income = capacity * 12
+elif screening_type == "Normal":
+    income = capacity * 7.50
+elif screening_type == "Discount":
+    income = capacity * 5
+
+print(f"{income:.2f} leva")
