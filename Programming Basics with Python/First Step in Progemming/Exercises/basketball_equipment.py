291574c0fe115726182f89f70952af3c141fb733
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2022-08-30 14:15:55 +0300

    Create basketball_equipment.py

---
+++ Programming Basics with Python/First Step in Progemming/Exercises/basketball_equipment.py
@@ -0,0 +1,9 @@
+year_tax = int(input())
+
+shoes_price = year_tax * 0.60
+suit = shoes_price * 0.80
+ball = suit / 4
+acc = ball / 5
+
+total_sum = year_tax + shoes_price + suit + ball + acc
+print(total_sum)
