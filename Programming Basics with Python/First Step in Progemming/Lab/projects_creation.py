6fbf28aef907d4fa94e6857146ec24a251457649
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2022-08-30 14:12:49 +0300

    Create projects_creation.py

---
+++ Programming Basics with Python/First Step in Progemming/Lab/projects_creation.py
@@ -0,0 +1,6 @@
+name = input()
+projects_count = int(input())
+hours = 3
+project_hours = projects_count * 3
+
+print(f"The architect {name} will need {project_hours} hours to complete {projects_count} project/s.")
