cdd047e6845f87d079c177197f3f9add7dd695c8
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2022-08-30 14:11:33 +0300

    Delete First steps in programming/Lab directory

--- First steps in programming/Lab/pet_shop.py
+++
@@ -1,9 +0,0 @@
-dogs_food = int(input())
-cats_food = int(input())
-
-dog_food_price = 2.50
-cat_food_price = 4
-
-total_sum = dogs_food * dog_food_price + cats_food * cat_food_price
-
-print(f"{total_sum} lv.")
