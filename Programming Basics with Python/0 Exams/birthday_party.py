3d2190f9150662141a80c9e0e9fbb866c68de8e9
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2022-09-06 15:08:25 +0300

    Create birthday_party.py

---
+++ Programming Basics with Python/0 Exams/birthday_party.py
@@ -0,0 +1,9 @@
+hall_rent = float(input())
+
+cake_price = hall_rent * 0.20
+drinks_price = cake_price * 0.55
+animator = hall_rent / 3
+
+total_price = hall_rent + cake_price + drinks_price + animator
+
+print(total_price)
